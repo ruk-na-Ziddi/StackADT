541fdb01f3446e5e89ccaaff6a515d997c75f5ac
Ankur Singh <ankurks@thoughtworks.com>
2015-01-31 14:35:57 +0530

    char type tests added for push

--- stackTest.c
+++ stackTest.c
@@ -11,8 +11,34 @@ void test_push_pushes_an_element_in_the_stack(){
 	int data=5;
 	Stack stack = createStack();
 	assertEqual(push(stack,&data), 1);
-	assertEqual(*(int*)(stack.list->tail->data),5);
+	assertEqual(*(int *)(stack.list->tail->data),5);
 	assertEqual(*((int *)(*stack.top)->data),5);
+}
+
+void test_push_pushes_an_char_element_in_the_stack(){
+	char data='a';
+	Stack stack = createStack();
+	assertEqual(push(stack,&data), 1);
+	assertEqual(*(char *)(stack.list->tail->data), 'a');
+	assertEqual(*((char *)(*stack.top)->data), 'a');
+}
+
+void test_push_pushes_two_element_in_the_stack(){
+	int first_node_data=5,second_node_data=10;
+	Stack stack = createStack();
+	assertEqual(push(stack,&first_node_data), 1);
+	assertEqual(push(stack,&second_node_data), 2);
+	assertEqual(*(int *)(stack.list->tail->data),10);
+	assertEqual(*((int *)(*stack.top)->data),10);
+}
+
+void test_push_pushes_two_char_element_in_the_stack(){
+	char first_node_data='a',second_node_data='b';
+	Stack stack = createStack();
+	assertEqual(push(stack,&first_node_data), 1);
+	assertEqual(push(stack,&second_node_data), 2);
+	assertEqual(*(char *)(stack.list->tail->data), 'b');
+	assertEqual(*((char *)(*stack.top)->data), 'b');
 }
 
 void test_pop_removes_the_current_top_of_stack_and_sets_previous_member_as_top(){
@@ -23,4 +49,14 @@ void test_pop_removes_the_current_top_of_stack_and_sets_previous_member_as_top()
 	assertEqual(*((int *)pop(stack)), second_node_data);
 	assertEqual(*(int*)(stack.list->tail->data),5);
 	assertEqual(*((int *)(*stack.top)->data),5);
+}
+
+void test_pop_removes_the_current_top_of_stack_and_sets_previous_member_as_top_for_character_data(){
+	char first_node_data='a',second_node_data ='b';
+	Stack stack = createStack();
+	push(stack,&first_node_data);
+	push(stack,&second_node_data);
+	assertEqual(*((char *)pop(stack)), second_node_data);
+	assertEqual(*(char *)(stack.list->tail->data),'a');
+	assertEqual(*((char *)(*stack.top)->data),'a');
 }
